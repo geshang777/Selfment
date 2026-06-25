0eaad99a216d996d4d37fdd940de74851ba01235
geshang777 <yzy12468@126.com>
2026-06-25 21:19:28 +0800

    [add] homepage

--- eval/__init__.py
+++
@@ -1,4 +0,0 @@
-from .metrics import compute_simple_metrics, compute_biref_metrics, compute_all_metrics
-
-__all__ = ["compute_simple_metrics", "compute_biref_metrics", "compute_all_metrics"]
-
--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .crf import dense_crf
-from .bilateral_solver import bilateral_solver_output
-
-__all__ = ["dense_crf", "bilateral_solver_output"]
-
